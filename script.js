f6d94423f29a6f0a5c20162ea300b67a40e9a477
ninaot <nissa.ot@gmx.fr>
2024-05-02 09:09:13 +0200

    2mai

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ AOS.init();
 // Envoie une requête GET à l'URL de l'api pour récupérer des données
 fetch("patisseries.json")
   // Une fois que la réponse est reçue
-  .then(rep => {
-    // transforme la reponse obtenu en JSON 
+  .then((rep) => {
+    // transforme la reponse obtenu en JSON
     return rep.json();
   })
   // Une fois que les données JSON sont extraites avec succès, jeles transmet à une fonction pour l'utiliser plus tard
-  .then(data => {
+  .then((data) => {
     // Appelle la fonction addProducts() avec les données JSON en entrée
     addProducts(data);
-    clients(data.entreprise.avantagesClients)
-    douce(data.entreprise.produits)
-    cours(data.entreprise.services)
-    avis(data.entreprise.temoignages)
+    clients(data.entreprise.avantagesClients);
+    douce(data.entreprise.produits);
+    cours(data.entreprise.services);
+    avis(data.entreprise.temoignages);
   });
 
 // Cette fonction prend les données JSON en entrée et effectue une action sur celles-ci
@@ -22,10 +22,9 @@ function addProducts(data) {
   console.log(data);
 }
 function addProducts(data) {
-    // Affiche les données dans la console du navigateur
-    console.log(data)
-document.querySelector("#user").innerHTML += 
-    ` 
+  // Affiche les données dans la console du navigateur
+  console.log(data);
+  document.querySelector("#user").innerHTML += ` 
     
     <div class="absolute">
     
@@ -33,82 +32,85 @@ document.querySelector("#user").innerHTML +=
       <p> ${data.entreprise.phraseAccroche}</p>
                 <a href="">${data.entreprise.texteAppelAction}</a>
                
-    </div>`
-  
-  }
-
-  
-function clients(avantagesclients) {let avantage = "";
-    avantagesclients.forEach(element => {
-        avantage += `
+    </div>`;
+}
+/*partie avantages clients
+ */
+function clients(avantagesclients) {
+  let avantage = "";
+  avantagesclients.forEach((element) => {
+    avantage += `
            
             <p>${element}</p>
             
         `;
-    });
-    document.querySelector("#large").innerHTML += avantage;
+  });
+  document.querySelector("#large").innerHTML += avantage;
 }
 
-  /*partie des produits avec les images et description*/
-function douce(produits){ 
-  let douceur="";
-  produits.forEach(tarte => {
-    douceur+=`
+/*partie des produits avec les images et description*/
+function douce(produits) {
+  let douceur = "";
+  produits.forEach((tarte) => {
+    douceur += `
       <div class="card">
-            <h3>${tarte.nom}</h3>
+           
            <div class="limite"> <img src="${tarte.images}" alt=""></div>
+           <h3>${tarte.nom}</h3>
             <p>${tarte.description}</p>
+            <a href="" class="cardbtn">Commander</a>
+           
         </div>
     
-    `
-    
+    `;
   });
   document.querySelector("#carte").innerHTML += douceur;
 }
 
 /*partie des services */
-function cours(services){
-  let traiteur="";
-  services.forEach(gateau => {
-    traiteur+=`
-    <div class="carte-service">
-    <h3>${gateau.nom}</h3>
+function cours(services) {
+  let traiteur = "";
+  services.forEach((gateau) => {
+    traiteur += `
+    <div class="carte-service"  data-aos="zoom-in-up">
     <img src="${gateau.image}" alt="">
-    <p>${gateau.description}</p>
+    <h3>${gateau.nom}</h3>
+       <p>${gateau.description}</p>
     <a href="">Réservez</a>
-    </div>
-    `
     
+    
+    </div>
+    `;
   });
   document.querySelector("#pflex").innerHTML += traiteur;
 }
 /*partie des témoignages*/
-function avis(temoignages){
-  let notes=""
-  temoignages.forEach(element => {
-    notes+=`
+function avis(temoignages) {
+  let notes = "";
+  temoignages.forEach((element) => {
+    notes += `
     <div class="carte-temoin">
+    <i class="fa-solid fa-quote-left"></i>
     <h3>${element.prenom}</h3>
-    <p>${element.typeExperience}</p>
+    <h4>${element.typeExperience}</h4>
     <p>${element.commentaire}</p>
-    <p>${element.note}</p>
+    <p class"avis"> note${element.note}</p>
+    <i class="fa-regular fa-face-smile"></i>
 
 </div>
     
-    `
-    
+    `;
   });
-  document.querySelector("#bflex").innerHTML +=notes ;
+  document.querySelector("#bflex").innerHTML += notes;
 }
-   
-    
-    
-    
-    
-  
-  
-   
 
 
+var map = L.map('map').setView([45, 47], 13);
 
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
+L.marker([3 ,34, 2]).addTo(map)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
